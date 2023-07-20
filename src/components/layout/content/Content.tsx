ede7a661f2fc6b73945b64d513277e381b584cc0
xtej4ik <chtejinna@gmail.com>
2023-07-20 17:58:40 +0100

    Projecft layout
    Date:      Thu Jul 17 09:21:50 2023 +0100

--- src/components/layout/content/Content.tsx
+++ src/components/layout/content/Content.tsx
@@ -1,10 +1,10 @@
-import ProjectList from '../../project-list/ProjectList'
-import styles from './Content.module.css'
+import ProjectList from "../../project-list/ProjectList";
+import styles from "./Content.module.css";
 
 export default function Content() {
-    return (
-        <main className={styles["content-container"]}>
-            <ProjectList />
-        </main>
-    )
+  return (
+    <main className={styles["content-container"]}>
+      <ProjectList />
+    </main>
+  );
 }
