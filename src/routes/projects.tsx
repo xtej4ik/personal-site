6257de3f3c9ae2d8677d7e61db701e0be4b71371
xtej4ik <chtejinna@gmail.com>
2023-07-26 18:27:07 +0100

    parallax effect on added to mobile

--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -1,5 +1,8 @@
 import commonStyles from '../common/common.module.css';
 
 export default function Projects() {
-  return <div className={commonStyles['page-common']}></div>;
+  return <div className={commonStyles['page-common']}>
+      <h2>My Projects</h2>
+  </div>;
+
 }
