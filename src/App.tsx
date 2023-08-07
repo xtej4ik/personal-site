fa8c117f78b099ea5093de5f0237324a26571d24
xtej4ik <chtejinna@gmail.com>
2023-08-07 16:30:31 +0100

    css cards for projects added

--- src/App.tsx
+++ src/App.tsx
@@ -5,41 +5,44 @@ import AboutMe from "./routes/about-me";
 import Projects from "./routes/projects";
 import Contact from "./routes/contact";
 
-const routes = [{
-  path: "/contact",
-  displayName: "Contact Me",
-  element: <Contact />,
-},
-{
-  path: "/projects",
-  displayName: "My Projects",
-  element: <Projects />,
-},
-{
-  path: "/",
-  displayName: "About Me",
-  element: <AboutMe />,
-}];
+const routes = [
+  {
+    path: "/contact",
+    displayName: "Contact Me",
+    element: <Contact />,
+  },
+  {
+    path: "/projects",
+    displayName: "My Projects",
+    element: <Projects />,
+  },
+  {
+    path: "/",
+    displayName: "About Me",
+    element: <AboutMe />,
+  },
+];
 
 function App() {
-
-
-
   return (
     <>
-      <Menu routes={routes} >
+      <Menu routes={routes}>
         <Routes>
-          {routes.map(route => {
-            return (<Route key={route.path} path={route.path} element={route.element} />);
+          {routes.map((route) => {
+            return (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.element}
+              />
+            );
           })}
         </Routes>
-        <Projects />
+
         <Footer />
       </Menu>
     </>
   );
 }
-
-
 
 export default App;
--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -1,66 +1,67 @@
 import commonStyles from "../common/common.module.css";
+import "./projects.css";
 
 export default function Projects() {
   return (
     <div className={commonStyles["page-common"]}>
-      <section>
-        <h2>Projects.</h2>
-      </section>
-
-      <section>
-        <h3>Backend News API</h3>
-        <div>
-          <a
-            href="https://newsbackend.inna.codes/api"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Live Demo
-          </a>
           <br />
-          <a
-            href="https://github.com/xtej4ik/BE-NC-NEWS-PROJECT"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub Repository
-          </a>
-        </div>
-      </section>
+      <h2>Projects</h2>
+      <div className="projects-container">
+        <section className="project-card">
+          <h3>Backend News API</h3>
+          <div>
+            <a
+              href="https://newsbackend.inna.codes/api"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>
+            <br />
+            <a
+              href="https://github.com/xtej4ik/BE-NC-NEWS-PROJECT"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub Repository
+            </a>
+          </div>
+        </section>
 
-      <section>
-        <h3>Frontend News Website</h3>
-        <div>
-          <a
-            href="https://newsfrontend.inna.codes/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Live Demo
-          </a>
-          <br />
-          <a
-            href="https://github.com/xtej4ik/nc-news"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub Repository
-          </a>
-        </div>
-      </section>
+        <section className="project-card">
+          <h3>Frontend News Website</h3>
+          <div>
+            <a
+              href="https://newsfrontend.inna.codes/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>
+            <br />
+            <a
+              href="https://github.com/xtej4ik/nc-news"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub Repository
+            </a>
+          </div>
+        </section>
 
-      <section>
-        <h3>Motive</h3>
-        <div>
-          <a
-            href="https://github.com/xtej4ik/motive-northcoders"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub Repository
-          </a>
-        </div>
-      </section>
+        <section className="project-card">
+          <h3>Motive</h3>
+          <div>
+            <a
+              href="https://github.com/xtej4ik/motive-northcoders"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub Repository
+            </a>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
