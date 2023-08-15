f86408920df7586fc1eb94007514f704b212f903
xtej4ik <chtejinna@gmail.com>
2023-08-15 21:01:25 +0100

    fixing deployment

--- src/components/layout/content/Content.tsx
+++ src/components/layout/content/Content.tsx
@@ -1,10 +1,5 @@
-
 import styles from "./Content.module.css";
 
 export default function Content() {
-  return (
-    <main className={styles["content-container"]}>
-      <ProjectList />
-    </main>
-  );
+  return <main className={styles["content-container"]}></main>;
 }
